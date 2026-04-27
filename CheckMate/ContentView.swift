50a02d04222839e5fe8ca5de8840ba2e6c29eeea
Alessandro Moreno <alessandro.moreno909@gmail.com>
2026-04-27 15:32:28 +0700

    Initialize Chess Board

--- CheckMate/ContentView.swift
+++ CheckMate/ContentView.swift
@@ -6,18 +6,24 @@
 //
 
 import SwiftUI
+import ChessboardKit
+import ChessKit
+
 
 struct ContentView: View {
+    @State var chessboardModel = ChessboardModel (fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world 123")
-        }
-        .padding()
+        Chessboard(chessboardModel: chessboardModel)
+            .onMove { move, isLegal, from, to, lan, promotionPiece in
+                print ("Move: \(lan)")
+            }
+            .frame(width: 300, height: 300)
     }
+    
 }
+
+
 
 #Preview {
     ContentView()
