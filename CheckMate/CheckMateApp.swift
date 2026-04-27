c0c2fe6178e2e19531e90f7b798f5760f56b4ed7
Alessandro Moreno <alessandro.moreno909@gmail.com>
2026-04-27 14:50:55 +0700

    Initial Commit

---
+++ CheckMate/CheckMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMateApp.swift
+//  CheckMate
+//
+//  Created by Alessandro Moreno Lawadinata on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CheckMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
