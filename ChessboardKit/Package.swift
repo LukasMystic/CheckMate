da1bd6e9cace7b5763920c03d7003c06572178e3
LukasMystic <lukasstanley42@gmail.com>
2026-04-28 13:47:28 +0700

    Add package dependencies

---
+++ ChessboardKit/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "ChessboardKit",
+    platforms: [.iOS(.v17), .macOS(.v14)],
+    products: [
+        .library(
+            name: "ChessboardKit",
+            targets: ["ChessboardKit"]),
+    ],
+    dependencies: [.package(url: "https://github.com/aperechnev/ChessKit", from: "1.3.7")],
+    targets: [
+        .target(name: "ChessboardKit",
+                dependencies: ["ChessKit"],
+                resources: [
+                    .process("Assets/Pieces/uscf")
+                ])
+    ],
+    swiftLanguageModes: [.v5, .v6]
+)
